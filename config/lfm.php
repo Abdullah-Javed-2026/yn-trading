ee5b3ca22c740b09024797b05906c6704b523ede
Muhammad Haroon <muhammadharoon02002@gmail.com>
2026-08-13 11:21:33 +0500

    feat: publish and configure laravel-filemanager package settings

--- config/lfm.php
+++ config/lfm.php
@@ -47,7 +47,7 @@ return [
 
     'folder_categories'        => [
         'file'  => [
-            'folder_name'  => 'files',
+            'folder_name'  => 'photos',
             'startup_view' => 'grid',
             'max_size'     => 50000, // size in KB
             'valid_mime'   => [
