02ac1a6a8ec1ad31eb9cd5c5fb14524fe729541c
Muhammad Haroon <muhammadharoon02002@gmail.com>
2026-08-13 10:56:19 +0500

    feat: configure AppServiceProvider for default string length and suppress deprecation warnings globally

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,6 +23,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        error_reporting(E_ALL & ~E_DEPRECATED & ~E_USER_DEPRECATED);
         Schema::defaultStringLength(191);
     }
 }
